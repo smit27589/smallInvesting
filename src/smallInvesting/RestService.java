7fd81718a181c4f587cbea713e70eb3cab73e6bc
smit27589 <smit27589@gmail.com>
2015-11-03 21:27:39 -0500

    First Commit

---
+++ src/smallInvesting/RestService.java
@@ -0,0 +1,10 @@
+package smallInvesting;
+
+public class RestService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("small Investing");
+	}
+
+}
